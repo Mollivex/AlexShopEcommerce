f92a9b06c2939c0fc6807e85c36d4580b98f692c
Alexandr Cocier <Limos12345@outlook.com>
2023-12-01 15:54:13 +0200

    Notification Toast Window

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,26 @@ const modal = document.querySelector('[data-modal]');
 const modalCloseBtn = document.querySelector('[data-modal-close]');
 const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
-//modal function
+// modal function
 const modalCloseFunc = function() {
     modal.classList.add('closed');
 }
 
-//modal eventListener
+// modal eventListener
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
 modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+
+
+// notification toast variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventListener
+toastCloseBtn.addEventListener('click', function () {
+  notificationToast.classList.add('closed');
+});
+
+
+
